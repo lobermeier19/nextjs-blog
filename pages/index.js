e74ef5dd3b845c3efab1ab1c8e2a346ee510d28c
Luke Obermeier <lobermeier19@Lukes-MacBook-Air.local>
2021-02-15 10:28:23 -0500

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>Luke's Website</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello! My name is Luke.</p>
+        <p>Hello! My name is Luke and this is my blog.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
